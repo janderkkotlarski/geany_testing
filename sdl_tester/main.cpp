6695135a634a408b10ad214f82bdf399c74b55ab
janderkkotlarski <j.d.kotlarski@gmx.com>
2023-02-22 18:57:00 +0100

    got a quit button to work, noice!

--- sdl_tester/main.cpp
+++ sdl_tester/main.cpp
@@ -21,6 +21,38 @@ int main(int, char **)
   SDL_CreateWindowAndRenderer(window_width, window_height, 0, &window, &renderer);
 
   SDL_RenderSetScale(renderer, 2, 2);
+  
+  bool looped
+  { true };
+  
+  
+  
+  while (looped)
+  {
+    SDL_Event event;
+    
+    while (SDL_PollEvent(&event))
+    {
+      switch (event.type)
+      {
+        case SDL_QUIT:
+          looped = false;
+          break;    
+          
+        case SDL_KEYDOWN:
+          switch(event.key.keysym.scancode)
+          {
+            case SDL_SCANCODE_ESCAPE:
+              looped = false;
+              break;    
+            default:
+              break;
+          }
+        default:
+            break;
+      }
+    }
+  }
 
   std::cout << "SDL seems to work!" << std::endl;
 
