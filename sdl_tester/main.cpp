2a9d915ed3f83916e3437c23086caa1f4888355d
janderkkotlarski <j.d.kotlarski@gmx.com>
2023-02-22 17:59:04 +0100

    somehow some simple library additions make it work and it concerns me that it is working that quickly and so simple and the sdl2 library shows me so

---
+++ sdl_tester/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+#include <SDL2/SDL.h>
+
+int main(int, char **)
+{
+  video cassette;
+
+  // cassette.display();
+
+  const int window_width
+  { 800 };
+
+  const int window_height
+  { 800 };
+
+  SDL_Init(SDL_INIT_EVERYTHING);
+
+  // SDL_Init(SDL _INIT_EVERYTHING);
+
+  SDL_Window* window
+  { nullptr };
+
+  SDL_Renderer* renderer
+  { nullptr };
+
+  SDL_CreateWindowAndRenderer(window_width, window_height, 0, &window, &renderer);
+
+  SDL_RenderSetScale(renderer, 2, 2);
+
+  std::cout << "Hello World!" << std::endl;
+
+  return 0;
+}
