ca44d8247496db20dff17a9497063791827c47e1
janderkkotlarski <j.d.kotlarski@gmx.com>
2023-05-29 00:08:32 +0200

    got some programming done while streaming on twitch and explaining how programming in c++ works

--- own_little_prog/main.cpp
+++ own_little_prog/main.cpp
@@ -1,6 +1,19 @@
-#include "little_helper.h"
+// #include "little_helper.h"
+
+#include <iostream>
 
 int main()
 {
-  type_hoi();
+  // type_hoi();
+  
+  unsigned number
+  { 1 };
+  
+  while (number)
+  {
+    ++number;
+    
+    if (number % 100000 == 0)
+    { std::cout << number << std::endl; }
+  }
 }
