74ed24985613df53582640f1c21ea8dc6583773e
janderkkotlarski <j.d.kotlarski@gmx.com>
2023-06-01 00:20:04 +0200

    got to explain about functions and .h and .cpp files

--- own_little_prog/main.cpp
+++ own_little_prog/main.cpp
@@ -1,19 +1,21 @@
-// #include "little_helper.h"
+#include "little_helper.h"
 
 #include <iostream>
 
+void type_what()
+{
+  std::cout << "What?!" << std::endl;
+  
+  const int count_max
+  { 1000000000 };
+  
+  for (int count{ 0 }; count < count_max; ++count)
+  {}
+}
+
 int main()
 {
-  // type_hoi();
+  type_hoi();
   
-  unsigned number
-  { 1 };
-  
-  while (number)
-  {
-    ++number;
-    
-    if (number % 100000 == 0)
-    { std::cout << number << std::endl; }
-  }
+  type_what();
 }
